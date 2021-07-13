dfeca753383c3eedd6a12e4a1403a6153e78a4b0
Jakubok <siekjakub5@gmail.com>
2021-07-13 17:55:50 +0200

    Add authorisation to the OrganisationResolver class

--- server/src/resolver/OrganisationResolver.ts
+++ server/src/resolver/OrganisationResolver.ts
@@ -1,8 +1,13 @@
 import { Authenticate } from "../middleware/authenticate";
-import { Arg, Field, ID, InputType, Mutation, Query, Resolver, UseMiddleware } from "type-graphql";
+import { Arg, Ctx, Field, InputType, Mutation, Query, Resolver, UseMiddleware } from "type-graphql";
 import { Organisation } from "../entity/Organisation";
 import { database } from "../app";
 import { UserInputError } from "apollo-server-express";
+import { Account } from "../entity/Account";
+import { AuthorisationError } from "../error/AuthorisationError";
+import { AuthorisationPack, CreateServereDataPack, RemoveServereDataPack, SeeDataPack, UpdateServereDataPack } from "../permissions/AuthorisationPacks";
+import { AppContext } from "../types";
+import { Permission } from "../permissions/Permission";
 
 @InputType()
 class OrganisationInput {
@@ -22,26 +27,57 @@ class OrganisationUpdateInput {
     description ?:string;
 }
 
+const organisationAuthorisation = async (
+    account :Account,
+    organisation :Organisation,
+    authorisationPack :AuthorisationPack
+) => {
+
+    if (account.permissionsManager.hasPermission(authorisationPack.everyone))
+        return authorisationPack.everyone;
+    
+    if (
+        account.permissionsManager.hasPermission(authorisationPack.organisation)
+        &&
+        organisation.id === (await account.person.organisation).id
+    )
+        return authorisationPack.organisation;
+
+    throw new AuthorisationError();
+}
+
 @Resolver(Organisation)
 export class OrganisationResolver {
 
     @UseMiddleware(Authenticate)
     @Query(() => [Organisation])
-    async organisations() {
-        return await database.getRepository(Organisation).find();
+    async organisations(
+        @Ctx() { account } :AppContext,
+    ) {
+
+        if (account?.permissionsManager.hasPermission(Permission.SEE_EVERYONES_DATA))
+            return await database.getRepository(Organisation).find();
+        
+        if (account?.permissionsManager.hasPermission(Permission.SEE_PEOPLES_IN_THE_SAME_ORGANISATION_DATA))
+            return await database.getRepository(Organisation).find({ where: { id: (await account.person.organisation).id } });
+
+        throw new AuthorisationError();
     }
 
     @UseMiddleware(Authenticate)
     @Query(() => Organisation)
     async organisation(
-        @Arg(`id`) ID :string
+        @Arg(`id`) ID :string,
+        @Ctx() { account } :AppContext,
     ) {
-        const organisation =  await database.getRepository(Organisation).findOne(ID);
+        const organisation = await database.getRepository(Organisation).findOne(ID);
 
         if (!organisation)
             throw new UserInputError(`Object not found`, {
                 argumentName: "id"
             });
+
+        organisationAuthorisation(account!, organisation, new SeeDataPack());
 
         return organisation;
     }
@@ -49,19 +85,24 @@ export class OrganisationResolver {
     @UseMiddleware(Authenticate)
     @Mutation(() => Organisation)
     async addOrganisation(
-        @Arg(`organisation`) organisationInput :OrganisationInput
+        @Arg(`organisation`) organisationInput :OrganisationInput,
+        @Ctx() { account } :AppContext,
     ) {
         const organisation = await Organisation.create({
             name: organisationInput.name,
             description: organisationInput.description
         });
+
+        organisationAuthorisation(account!, organisation, new CreateServereDataPack());
+
         return await organisation.save();
     }
 
     @UseMiddleware(Authenticate)
     @Mutation(() => Organisation)
     async updateOrganisation(
-        @Arg(`organisation`) update :OrganisationUpdateInput
+        @Arg(`organisation`) update :OrganisationUpdateInput,
+        @Ctx() { account } :AppContext,
     ) {
         const organisation = await database.getRepository(Organisation).findOne(update.id);
 
@@ -75,18 +116,23 @@ export class OrganisationResolver {
         if (update.description)
             organisation.description = update.description;
 
+        organisationAuthorisation(account!, organisation, new UpdateServereDataPack());
+
         return await organisation.save();
     }
 
     @UseMiddleware(Authenticate)
     @Mutation(() => Boolean)
     async removeOrganisation(
-        @Arg(`id`) ID :string
+        @Arg(`id`) ID :string,
+        @Ctx() { account } :AppContext,
     ) {
         const organisation = await database.getRepository(Organisation).findOne(ID);
 
         if (!organisation)
             return false;
+
+        organisationAuthorisation(account!, organisation, new RemoveServereDataPack());
         
         await organisation.beforeRemove();
         await organisation.remove();
