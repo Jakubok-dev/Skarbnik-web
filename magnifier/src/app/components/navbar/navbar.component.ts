03bff0f25f0bd813a0373a47d76a78fba97c8ba9
Jakubok <siekjakub5@gmail.com>
2021-07-17 19:01:43 +0200

    Design changes

--- magnifier/src/app/components/navbar/navbar.component.ts
+++ magnifier/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { ElementRef, HostBinding, ViewEncapsulation } from '@angular/core';
+import { ElementRef } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { faMoon, faSun } from '@fortawesome/free-regular-svg-icons';
 import { ThemeService } from 'src/app/services/theme/theme.service';
@@ -7,19 +7,20 @@ import { ThemeService } from 'src/app/services/theme/theme.service';
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
-  encapsulation: ViewEncapsulation.None
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(private themeService :ThemeService) { }
+  constructor(private themeService :ThemeService, private elementRef: ElementRef) { }
 
   ngOnInit(): void {
+    this.themeService.loadTheme(this.elementRef);
   }
 
   get darkMode() { return this.themeService.darkMode; }
 
   toggleDarkMode() {
     this.themeService.darkMode = !this.themeService.darkMode;
+    this.themeService.loadTheme(this.elementRef);
   }
 
   faMoon = faMoon;
