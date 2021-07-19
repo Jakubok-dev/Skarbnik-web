d5021b1021174bc3680040388192d9aa75ed8c39
Jakubok <siekjakub5@gmail.com>
2021-07-19 14:38:21 +0200

    Add theme caching

--- magnifier/src/app/services/theme/theme.service.ts
+++ magnifier/src/app/services/theme/theme.service.ts
@@ -1,11 +1,15 @@
 import { ElementRef, Injectable } from '@angular/core';
+import { CookiesService } from '../cookies/cookies.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ThemeService {
 
-  constructor() { }
+  constructor(private cookiesService :CookiesService) {
+    if (cookiesService.getCookie(`dark-mode`) === 'false')
+      this.darkMode = false;
+  }
 
   darkMode = true;
 
@@ -19,4 +23,21 @@ export class ThemeService {
     elementRef.nativeElement.ownerDocument.body.classList.remove(`theme-dark`);
     elementRef.nativeElement.ownerDocument.body.classList.add(`theme-light`);
   }
+
+  toggleTheme(elementRef: ElementRef) {
+    this.darkMode = !this.darkMode;
+    this.cookiesService.setCookie(`dark-mode`, booleanToString(this.darkMode));
+
+    if (this.darkMode) {
+      elementRef.nativeElement.ownerDocument.body.classList.remove(`theme-light`);
+      elementRef.nativeElement.ownerDocument.body.classList.add(`theme-dark`);
+      return;
+    }
+
+    elementRef.nativeElement.ownerDocument.body.classList.remove(`theme-dark`);
+    elementRef.nativeElement.ownerDocument.body.classList.add(`theme-light`);
+  }
 }
+
+
+const booleanToString = (bool :boolean) => bool === true ? 'true' : 'false';
